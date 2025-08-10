8c2ec61db9e7828d5518c824717c951df410a31d
maxrumbo <maxrumbo06@gmail.com>
2025-08-11 02:41:24 +0700

    delete trash

--- isengaja/routes/web.php
+++ isengaja/routes/web.php
@@ -1,11 +1,4 @@
-use App\Http\Controllers\PortfolioController;
 
-// Route untuk user (frontend)
-Route::get('/', [PortfolioController::class, 'index'])->name('portfolio.index');
-
-// Route untuk admin (backend)
-Route::get('admin/portfolio', [PortfolioController::class, 'adminIndex'])->name('admin.portfolio.index');
-Route::resource('admin/portfolio', PortfolioController::class)->except(['index', 'show']);
 <?php
 
 use Illuminate\Support\Facades\Route;
